092ebdb01bcf8e26a0d55abec7539e341cc31c2d
Melanie <melanie.collins.p@gmail.com>
2024-02-29 11:25:39 -0300

    primer ejercicio

---
+++ assets/js/ejercicio1.js
@@ -0,0 +1,41 @@
+let form = document.getElementById("formulario");
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    limpiarErrores();
+    let textNombre = document.querySelector("#nombre").value;
+    let textAsunto = document.querySelector("#asunto").value;
+    let textMensaje = document.querySelector("#mensaje").value;
+    let resultado = validar(textNombre, textAsunto, textMensaje);
+    if (resultado == true) { exito(); };
+});
+
+function limpiarErrores() {
+    document.querySelector(".resultado").innerHTML = "";
+    document.querySelector(".errorNombre").innerHTML = "";
+    document.querySelector(".errorAsunto").innerHTML = "";
+    document.querySelector(".errorMensaje").innerHTML = "";
+};
+
+function exito() {
+    document.querySelector(".resultado").innerHTML = "Mensaje enviado con éxito!!!";
+};
+
+function validar(nombre, asunto, mensaje) {
+    let pasamosLaValidacion = true;
+    let validacionNombre = /^[A-Za-z]+$/;
+    if (validacionNombre.test(nombre) == false) {
+        document.querySelector(".errorNombre").innerHTML = "El nombre es requerido"
+        pasamosLaValidacion = false;
+    };
+    let validacionAsunto = /^[A-Za-z]+$/;
+    if (validacionAsunto.test(asunto) == false) {
+        document.querySelector(".errorAsunto").innerHTML = "El asunto es requerido"
+        pasamosLaValidacion = false;
+    };
+    let validaciónMensaje = /^[A-Za-z]+$/;
+    if (validaciónMensaje.test(mensaje) == false) {
+        document.querySelector(".errorMensaje").innerHTML = "El mensaje es requerido"
+        pasamosLaValidacion = false;
+    };
+    return pasamosLaValidacion;
+};
