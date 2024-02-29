517bda35cf9d5e0bce455bae5f322137ff398ffa
Melanie <melanie.collins.p@gmail.com>
2024-02-29 13:23:23 -0300

    ejercicio 2

---
+++ assets/js/ejercicio2.js
@@ -0,0 +1,57 @@
+const caja = document.querySelector("#caja")
+const btn1 = document.getElementById('btn-1');
+const btn2 = document.getElementById('btn-2');
+const btn3 = document.getElementById('btn-3');
+const btn4 = document.getElementById('btn-4');
+const btn5 = document.getElementById('btn-5');
+const btn6 = document.getElementById('btn-6');
+
+
+
+btn1.addEventListener('click', function onClick(event) {
+    // 👇️ change background color
+    caja.style.backgroundColor = '#e53e3e';
+
+    // 👇️ optionally change text color
+    // document.body.style.color = 'white';
+});
+
+btn2.addEventListener('click', function onClick(event) {
+    // 👇️ change background color
+    caja.style.backgroundColor = '#dd6b20';
+
+    // 👇️ optionally change text color
+    // document.body.style.color = 'white';
+});
+
+btn3.addEventListener('click', function onClick(event) {
+    // 👇️ change background color
+    caja.style.backgroundColor = '#faf089';
+
+    // 👇️ optionally change text color
+    // document.body.style.color = 'white';
+});
+
+btn4.addEventListener('click', function onClick(event) {
+    // 👇️ change background color
+    caja.style.backgroundColor = '#48bb78';
+
+    // 👇️ optionally change text color
+    // document.body.style.color = 'white';
+});
+
+btn5.addEventListener('click', function onClick(event) {
+    // 👇️ change background color
+    caja.style.backgroundColor = '#81e6d9';
+
+    // 👇️ optionally change text color
+    // document.body.style.color = 'white';
+});
+
+btn6.addEventListener('click', function onClick(event) {
+    // 👇️ change background color
+    caja.style.backgroundColor = '#d53f8c';
+
+    // 👇️ optionally change text color
+    // document.body.style.color = 'white';
+});
